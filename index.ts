33414bf93da2b530e3bf5115f86316f6dadbc414
msys <irshad89434@gmail.com>
2024-08-23 19:48:58 +0530

    updates

--- index.ts
+++ index.ts
@@ -4,3 +4,5 @@ export * from "./src/errors/not-found-error";
 
 export * from "./src/middlewares/validate-request";
 export * from "./src/middlewares/error-handler";
+
+export * from "./src/functions/jwt";
---
+++ src/functions/jwt.ts
@@ -0,0 +1,31 @@
+import Jwt from "jsonwebtoken"
+
+export interface JWTUserPayload{
+    user: string;
+    role: string;
+    organization: string;
+}
+
+export const generateJwtAccessToken = (payload: JWTUserPayload, secret: string) =>{
+    return Jwt.sign(
+        payload,
+        secret,
+        {
+            expiresIn:"1h"
+        }
+    )
+}
+
+export const generateJwtRefreshToken = (payload: JWTUserPayload, secret: string)=>{
+    return Jwt.sign(
+        payload,
+        secret,
+        {
+            expiresIn: "15d"
+        }
+    )
+}
+
+export const verifyJwt = (token: string, secret: string): JWTUserPayload =>{
+    return Jwt.verify(token, secret) as JWTUserPayload;
+}
