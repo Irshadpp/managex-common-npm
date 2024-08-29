13d6734b8321491924e803c567233dc202bc88bc
msys <irshad89434@gmail.com>
2024-08-29 16:15:20 +0530

    updates

--- src/functions/jwt.ts
+++ src/functions/jwt.ts
@@ -11,7 +11,7 @@ export const generateJwtAccessToken = (payload: JWTUserPayload, secret: string) 
         payload,
         secret,
         {
-            expiresIn:"30s"
+            expiresIn:"5m"
         }
     )
 }
