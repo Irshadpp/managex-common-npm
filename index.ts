a117a84c364a08569dff4893f60ecbdab0f53cba
msys <irshad89434@gmail.com>
2024-08-29 11:22:36 +0530

    updates

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ export * from "./src/errors/request-validation-error";
 export * from "./src/errors/bad-request-error";
 export * from "./src/errors/not-found-error";
 export * from "./src/errors/not-authorized-error";
+export * from "./src/errors/for-bidden-error";
 
 export * from "./src/middlewares/validate-request";
 export * from "./src/middlewares/error-handler";
