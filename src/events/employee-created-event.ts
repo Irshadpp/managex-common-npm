d692cdb01867d7ef0a9f868851a812e6b9aca38d
msys <irshad89434@gmail.com>
2024-08-30 21:22:46 +0530

    updates

--- src/events/employee-created-event.ts
+++ src/events/employee-created-event.ts
@@ -11,7 +11,7 @@ interface Address{
     zipcode: string;
 }
 
-export interface EmployeeCreatedEvnet{
+export interface EmployeeCreatedEvent{
     queue: Queues.EmployeeCreated;
     data:{
         id: string;
