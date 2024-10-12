54d3b7f79617d797d0330b97bd44a792272a56a9
msys <irshad89434@gmail.com>
2024-10-12 10:46:34 +0530

    updates

--- src/functions/send-response.ts
+++ src/functions/send-response.ts
@@ -10,6 +10,17 @@ export enum HttpStatusCode {
     NOT_FOUND = 404,
     INTERNAL_SERVER_ERROR = 500,
   }
+
+  export const CommonMessages = {
+    SUCCESS: "Operation successful",
+    CREATED: "Resource successfully created",
+    VERIFIED: "Resource verified",
+    BAD_REQUEST: "Bad request",
+    UNAUTHORIZED: "Unauthorized access",
+    FORBIDDEN: "Access forbidden",
+    NOT_FOUND: "Resource not found",
+    INTERNAL_SERVER_ERROR: "Internal server error",
+  };
   
   export const sendResponse = <T>(
     res: Response, 
