9915430499d1c342a4df1d09c9a15d346d3084e6
msys <irshad89434@gmail.com>
2024-10-18 11:53:38 +0530

    updates

--- src/config/rabbitmq.ts
+++ src/config/rabbitmq.ts
@@ -5,6 +5,7 @@ let connection: amqp.Connection | null;
 export const connectToRabbitMQ = async (): Promise<amqp.Channel> =>{
     if(!connection){
         // connection = await amqp.connect('amqp://localhost');
+        console.log(process.env.RABBITMQ_URL, "ennnnvvvvvvvvvvvvvvvvvvvv---------------<")
         connection = await amqp.connect('amqp://rabbitmq-srv.default.svc.cluster.local');
     }
     return await connection.createChannel();
