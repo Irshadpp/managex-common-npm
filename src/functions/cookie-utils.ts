d9f67584f6c2de1a78a3b6c1f6d5f73a06a9fe3d
msys <irshad89434@gmail.com>
2024-10-27 13:59:52 +0530

    updates

--- src/functions/cookie-utils.ts
+++ src/functions/cookie-utils.ts
@@ -1,18 +1,23 @@
-import { Request, Response } from "express"
+import { Request, Response } from "express";
 
-export const setCookie = (res: Response, name: string, value: string, options: any): void =>{
-    res.cookie(name, value, {
-        httpOnly: true, 
-        secure: process.env.NODE_ENV === 'production' ? 'strict' : 'lax',
-        sameSite: 'strict',
-        ...options
-    });
-}
+export const setCookie = (
+  res: Response,
+  name: string,
+  value: string,
+  options: any
+): void => {
+  res.cookie(name, value, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "strict" : "lax",
+    ...options,
+  });
+};
 ////
-export const clearCookie = (res: Response, name: string): void =>{
-    res.cookie(name, '', {maxAge: 0});
-}
+export const clearCookie = (res: Response, name: string): void => {
+  res.cookie(name, "", { maxAge: 0 });
+};
 
-export const getCookie = (req: Request, name: string): string | undefined =>{
-    return req.cookies[name];
+export const getCookie = (req: Request, name: string): string | undefined => {
+  return req.cookies[name];
 };
