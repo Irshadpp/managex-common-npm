95d2892390f8c26bfb9ee39d4963c55f2139849c
msys <irshad89434@gmail.com>
2024-10-30 01:34:20 +0530

    updates

--- src/events/user-updated-event.ts
+++ src/events/user-updated-event.ts
@@ -19,6 +19,7 @@ export interface UserUpdatedEvent{
         username?: string;
         phone?: number;
         role?: Role;
+        isActive?: boolean;
         profileURL?: string;
         dob?: Date;
         address?: Address;
