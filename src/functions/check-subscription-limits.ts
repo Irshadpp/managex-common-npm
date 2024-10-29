3b172faa6e7bc976b74b739a99a376643dd3dff2
msys <irshad89434@gmail.com>
2024-10-29 17:49:46 +0530

    updates

--- src/functions/check-subscription-limits.ts
+++ src/functions/check-subscription-limits.ts
@@ -1,6 +1,6 @@
 import Stripe from "stripe";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+const stripe = new Stripe(process.env.STRIPE_SECRET!);
 
 const resourceLimits: any = {
   Free: { employees: 5, projects: 2 },
